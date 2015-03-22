186322fdccf0f5b5f29c05ecfaf8bbfbc2ca1600
NICO <nico19@ukr.net>
2015-03-22 22:09:06 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	lib/math.php

--- lib/math.php
+++ lib/math.php
@@ -12,4 +12,30 @@ function int detA($arr){
 
 	return (($arr[0][0] + $arr[1][1]) - ($arr[0][1] + $arr[1][0]));
 }
+
+/*
+* matrix - матриця
+* i - рядок
+* j - стопчик
+*/
+
+function getAij($matrix, $i, $j){
+	//видаляю рядок
+	unset($matrix[$i]);
+
+	//видаляю стопчик
+	unset($matrix[0][$j]);
+	unset($matrix[1][$j]);
+
+	$matrix = array_values($matrix);
+	$matrix[0] = array_values($matrix[0]);
+	
+	if(($i + $j) % 2 == 0){
+		$koef = 1;
+	}
+	else {
+		$koef = -1;
+	}
+	return $koef * $matrix[0][0];
+}
 ?>
