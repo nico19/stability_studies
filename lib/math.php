40952b9927d529bfe798ad9aaccb0e478ec1032a
tolyan---tolyan <tolyan---tolyan@ukr.net>
2015-03-22 21:24:33 +0200

    add function detA
    return determinante matrix A

--- lib/math.php
+++ lib/math.php
@@ -7,4 +7,9 @@ function calcF1($x1, $x2){
 function calcF2($x1, $x2){
 	return ((-4 * pow($x1, 2)) + (8 * $x2) +1);
 }
+
+function int detA($arr){
+
+	return (($arr[0][0] + $arr[1][1]) - ($arr[0][1] + $arr[1][0]));
+}
 ?>
